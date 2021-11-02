0a5a16b69cfff70b669c85c76fccbf0ef06e628c
strugglebak <strugglebak@gmail.com>
2021-11-02 20:30:24 +0800

    feat: rebuild hello-cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-d8aed04a58286d75.d
@@ -0,0 +1,5 @@
+/mnt/d/newHome/repo/rust-demo/hello-cargo/target/debug/deps/hello_cargo-d8aed04a58286d75.rmeta: src/main.rs
+
+/mnt/d/newHome/repo/rust-demo/hello-cargo/target/debug/deps/hello_cargo-d8aed04a58286d75.d: src/main.rs
+
+src/main.rs:
