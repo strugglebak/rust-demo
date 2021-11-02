852e2ac600c7549b5b724329817c5597fdde65f6
strugglebak <strugglebak@gmail.com>
2021-11-02 19:47:32 +0800

    feat: new hello cargo

---
+++ hello-cargo/target/debug/deps/hello_cargo-53e86a7c78a6cbbf.d
@@ -0,0 +1,5 @@
+/mnt/d/newHome/repo/rust-demo/hello-cargo/target/debug/deps/hello_cargo-53e86a7c78a6cbbf.rmeta: src/main.rs
+
+/mnt/d/newHome/repo/rust-demo/hello-cargo/target/debug/deps/hello_cargo-53e86a7c78a6cbbf.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/mnt/d/newHome/repo/rust-demo/hello-cargo/target/debug/hello-cargo: /mnt/d/newHome/repo/rust-demo/hello-cargo/src/main.rs
