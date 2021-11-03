6bb39b5a19e13540b5c4b614d577fed8ac977229
strugglebak <strugglebak@gmail.com>
2021-11-03 19:53:16 +0800

    feat: add shadows

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -7,4 +7,13 @@ fn main() {
 
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is {}", THREE_HOURS_IN_SECONDS);
+
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {}", y);
+    }
+    println!("The value of y is: {}", y);
 }
