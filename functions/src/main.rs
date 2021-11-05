de3c48b242a15cf7c367b90053d8c740bfe1ba75
strugglebak <strugglebak@gmail.com>
2021-11-05 19:37:12 +0800

    feat: add arrow to define a function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,14 +2,18 @@ fn main() {
     println!("Hello, world!");
     xxx(111, 'o');
 
-    let x = 5;
+    let _x = 5;
     let y = {
         let x = 4;
         x + 1
     };
     println!("The value of y is {}", y);
+
+    println!("The five function return {}", five());
 }
 
 fn xxx(x: i32, y: char) {
     println!("This xxx function, parameter is {} {}", x, y);
 }
+
+fn five() -> i32 { 5 }
