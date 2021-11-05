07ac4524f49dd65ac9270d95d04bd2113aa1fa89
strugglebak <strugglebak@gmail.com>
2021-11-05 19:09:23 +0800

    feat: add integer overflow

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     let guess: u32 = "42".parse().expect("Not a number!");
     // error
     // let guess = "42".parse().expect("Not a number!");
+
+    // error integer overflow
+    // let guess: u8 = 257;
     println!("guess value is {}", guess);
 }
