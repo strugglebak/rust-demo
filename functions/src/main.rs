c5470208843b91daa731eb64569ebe35f75dad60
strugglebak <strugglebak@gmail.com>
2021-11-05 19:33:24 +0800

    feat: {} scope return a value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     println!("Hello, world!");
     xxx(111, 'o');
+
+    let x = 5;
+    let y = {
+        let x = 4;
+        x + 1
+    };
+    println!("The value of y is {}", y);
 }
 
 fn xxx(x: i32, y: char) {
