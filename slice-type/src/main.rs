bc591bb94bfa1581d5b5b514152ac59d4a58f870
strugglebak <strugglebak@gmail.com>
2021-11-06 21:55:08 +0800

    feat: add other slice

--- slice-type/src/main.rs
+++ slice-type/src/main.rs
@@ -27,6 +27,10 @@ fn main() {
     // which is a immutable reference
 
     s.clear();
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2,3]);
 }
 
 // fn first_world(s: &String) -> &str {
