cef4c4b0b3639c64076c77b1c5927ee7047e46c2
strugglebak <strugglebak@gmail.com>
2021-11-06 17:05:44 +0800

    feat: ways variables and data interact: move

--- understanding-ownership/src/main.rs
+++ understanding-ownership/src/main.rs
@@ -17,4 +17,12 @@ fn main() {
         // s here is valid
         let _s = String::from("hello world");
     }   // the scope is over, s is no longer valid
+        // and call the drop function
+
+    let s1 = String::from("hello");
+    // just copy the reference of s1
+    let _s2 = s1;
+
+    // error: s1 no longer be valid
+    // println!("{}, world", s1);
 }
