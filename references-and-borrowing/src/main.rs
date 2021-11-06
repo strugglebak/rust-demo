f874c9c5a9ba36c7f46b5d76da16e308a32a67ca
strugglebak <strugglebak@gmail.com>
2021-11-06 20:04:57 +0800

    feat: add mutable refercences

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -7,12 +7,43 @@ fn main() {
     // not allowed to modify sth we have a reference to
     // let s2 = String::from("hello");
     // change(&s2);
+
+    let mut s2 = String::from("hello");
+    change(&mut s2);
+
+    println!("s2 is {}", s2);
+
+    // will be error
+    // only one mutable reference can assign to a variable at a time
+    // prevent data race
+    // let mut s = String::from("hello");
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+
+    // will be error
+    // let mut s = String::from("hello");
+    // let r1 = &s;      // ok
+    // let r2 = &s;      // ok
+    // let r3 = &mut s;  // error!!!
+    // println!("{}, {}, {}", r1, r2, r3);
+
+    let mut s = String::from("hello");
+    let r1 = &s;      // ok
+    let r2 = &s;      // ok
+    println!("{}, {}", r1, r2);
+    let r3 = &mut s;  // ok
+    println!("{}", r3);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
-fn change(s: &String) {
+// fn change(s: &String) {
+//     s.push_str(", world")
+// }
+
+fn change(s: &mut String) {
     s.push_str(", world")
 }
