eb4bf5134f5cae3895723d10a920e1d5ca1bb193
strugglebak <strugglebak@gmail.com>
2021-11-07 21:24:26 +0800

    feat: using tuple structs without named fields to create different types

--- defining-and-instantiating-structs/src/main.rs
+++ defining-and-instantiating-structs/src/main.rs
@@ -25,6 +25,15 @@ fn main() {
     //     ..user1
     // };
     // println!("user1.email is {}", user1.email);
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("black: {}, {}, {}", black.0, black.1, black.2);
+    println!("origin: {}, {}, {}", origin.0, origin.1, origin.2);
 }
 
 struct User {
