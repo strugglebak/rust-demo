f695486a8d9e0f5f0dbe2f5292d82987488659f2
strugglebak <strugglebak@gmail.com>
2021-11-08 21:25:57 +0800

    feat: catch-all patterns and the _ placeholder

--- the-match-control-flow-operator/src/main.rs
+++ the-match-control-flow-operator/src/main.rs
@@ -20,7 +20,22 @@ fn main() {
     let five = Some(5);
     let _six = plus_one(five);
     let _none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // other => move_player(other),
+        // _ => reroll(),
+        // we don't want to run any code in this case
+        _ => ()
+    }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+// fn move_player(num_spaces: u8) {}
+// fn reroll() {}
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
