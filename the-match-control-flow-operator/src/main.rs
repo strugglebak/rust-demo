7ea1d04c46c3bb389b41425712410b990ee8109b
strugglebak <strugglebak@gmail.com>
2021-11-08 19:45:59 +0800

    feat: add patterns that bind to values

--- the-match-control-flow-operator/src/main.rs
+++ the-match-control-flow-operator/src/main.rs
@@ -1,13 +1,21 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     println!("penny: {}", value_in_cents(Coin::Penny));
     println!("nickel: {}", value_in_cents(Coin::Nickel));
+    println!("quarter: {}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+    println!("quarter: {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -18,6 +26,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:#?}!", state);
+            25
+        },
     }
 }
