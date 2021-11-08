abe525b33287e819b2ade2871c75256e6603ecef
strugglebak <strugglebak@gmail.com>
2021-11-08 17:32:19 +0800

    feat: add enum value

--- defining-an-enum/src/main.rs
+++ defining-an-enum/src/main.rs
@@ -1,8 +1,36 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    // V4(String),
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
+// the same as above
+// struct QuitMessage; // unit struct
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
 fn main() {
-    println!("Hello, world!");
+    // let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let _home = IpAddrKind::V4(127, 0, 0, 1);
+    let _loopback = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
