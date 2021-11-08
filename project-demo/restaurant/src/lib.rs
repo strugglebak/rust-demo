b960ee277f9599e59e0cb0b44019e7a432110952
strugglebak <strugglebak@gmail.com>
2021-11-08 23:19:18 +0800

    feat: starting relative paths with super

--- project-demo/restaurant/src/lib.rs
+++ project-demo/restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+fn serve_order() {}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -9,6 +11,14 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {
