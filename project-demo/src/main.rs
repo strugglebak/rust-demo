4870396bd2dd3da3c1e4820aa1eb3298e93a9087
strugglebak <strugglebak@gmail.com>
2021-11-09 17:47:08 +0800

    feat: add glob operator

--- project-demo/src/main.rs
+++ project-demo/src/main.rs
@@ -2,6 +2,9 @@ use rand::Rng;
 
 use std::{cmp::Ordering, io};
 
+// be careful using it !!!
+use std::collections::*;
+
 fn main() {
   let secret_number = rand::thread_rng().gen_range(1..101);
   println!("The value is {}", secret_number);
