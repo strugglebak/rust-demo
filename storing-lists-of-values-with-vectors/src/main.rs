af03d09a3d30ed603d9ab1ecc12bd9048496e293
strugglebak <strugglebak@gmail.com>
2021-11-10 16:22:44 +0800

    feat: updating a vector

--- storing-lists-of-values-with-vectors/src/main.rs
+++ storing-lists-of-values-with-vectors/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    // v value changed, so we need to make it mutable
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(4);
+
     // vec! is a macro
     let a = vec![1, 2, 3];
 }
