f9bd106c6dc6a1cb1261c559f5444d4665a9d306
strugglebak <strugglebak@gmail.com>
2021-11-12 00:22:17 +0800

    feat: fixing the largest function with trait bounds

--- generic-data-types/src/main.rs
+++ generic-data-types/src/main.rs
@@ -15,17 +15,18 @@ struct Point<T, U> {
 // }
 
 // using generic types in function definitions
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
+// or Clone is Ok, but it will be slower than Copy
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-//     for &item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
 
-//     largest
-// }
+    largest
+}
 
 impl<T, U> Point<T, U> {
     fn x(&self) -> &T {
@@ -50,6 +51,16 @@ impl Point<f32, f32> {
 }
 
 fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
     let integer = Point { x: 5, y: 5.0 };
     let float = Point { x: 1.0, y: 1 };
     let string = Point {x: 'x', y: "hello world "};
