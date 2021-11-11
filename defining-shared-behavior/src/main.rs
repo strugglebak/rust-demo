39c608b9015726dbdf5ba000d88d834eab067a78
strugglebak <strugglebak@gmail.com>
2021-11-12 00:31:36 +0800

    feat: conditionally implement methods on a generic type depending on trait bounds

--- defining-shared-behavior/src/main.rs
+++ defining-shared-behavior/src/main.rs
@@ -99,8 +99,33 @@ fn returns_summarizable(switch: bool) -> impl Summary {
     }
 }
 
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
 
 fn main() {
+
+    let pair = Pair::new(1, 2);
+    pair.cmp_display();
+
     let tweet = returns_summarizable(true);
 
     println!("1 new tweet: {}", tweet.summarize());
