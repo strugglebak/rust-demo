ef18696c7305fe796fb167d937eeb4817a0e6793
strugglebak <strugglebak@gmail.com>
2021-11-12 00:07:16 +0800

    feat: returning types that implement traits

--- defining-shared-behavior/src/main.rs
+++ defining-shared-behavior/src/main.rs
@@ -61,29 +61,51 @@ impl Summary for Tweet {
 
 // }
 
+fn returns_summarizable(switch: bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+    } else {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+        // error:
+        // does not allowed
+        // Tweet {
+        //     username: String::from("horse_ebooks"),
+        //     content: String::from(
+        //         "of course, as you probably already know, people",
+        //     ),
+        //     reply: false,
+        //     retweet: false,
+        // }
+    }
+}
 
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
-        reply: false,
-        retweet: false,
-    };
+    let tweet = returns_summarizable(true);
 
     println!("1 new tweet: {}", tweet.summarize());
 
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
-    };
+    let article = returns_summarizable(false);
 
     println!("New article available! {}", article.summarize());
 }
