96a8ee9b788532d51992e991d9c24d8c0e9503cf
strugglebak <strugglebak@gmail.com>
2021-11-11 11:28:37 +0800

    chore: add annotation

--- storing-uft8-encoded-text-with-strings/src/main.rs
+++ storing-uft8-encoded-text-with-strings/src/main.rs
@@ -29,4 +29,26 @@ fn main() {
 
     s1.push('X');
     println!("s1 is {}", s1);
+
+    // + operator looks like this
+    // fn add(self, s: &str) -> String {
+    // but the type of `&s2` is `&String`, not the `&str`
+    // so + operator force the `&String` argument into a `&str`
+    // turns `&s2` into `&s2[...]`
+    // + operator take ownership of `self`, because `self` doesn't have an `&`
+    let s3 = s1 + &s2;
+    println!("s3 is {}", s3);
+
+    // error:
+    // s1 has been moved here and can no longer be used
+    // println!("s1 is {}", s1);
+    let s4 = String::from("4");
+    let s5 = String::from("5");
+    let s6 = String::from("6");
+    let s7 = s4 + "-" + &s5 + "-" + &s6;
+    println!("s7 is {}", s7);
+    // use format! macro
+    // and this call doesn’t take ownership of any of its parameters
+    let s8 = format!("{}-{}-{}", s5, s6, s7);
+    println!("s8 is {}", s8);
 }
