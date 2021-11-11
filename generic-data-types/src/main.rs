79eb8a491badc72c1d2587dc277bde6039b89387
strugglebak <strugglebak@gmail.com>
2021-11-11 20:16:24 +0800

    feat: using generic types in method definitions

--- generic-data-types/src/main.rs
+++ generic-data-types/src/main.rs
@@ -27,9 +27,36 @@ struct Point<T, U> {
 //     largest
 // }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+    fn y(&self) -> &U {
+        &self.y
+    }
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+// this will be OK
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let integer = Point { x: 5, y: 5.0 };
     let float = Point { x: 1.0, y: 1 };
+    let string = Point {x: 'x', y: "hello world "};
     println!("{:?}", integer);
     println!("{:?}", float);
+    println!("{} : {}", float.x(), float.y());
+
+    let p = integer.mixup(string);
+    println!("{:?}", p);
 }
