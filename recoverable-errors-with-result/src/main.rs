99427fa58f260a0678cacd977be9a23d809c4d5e
strugglebak <strugglebak@gmail.com>
2021-11-11 16:04:52 +0800

    feat: matching on different errors

--- recoverable-errors-with-result/src/main.rs
+++ recoverable-errors-with-result/src/main.rs
@@ -1,10 +1,34 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let _f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        // Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
     };
+
+    let _f1 = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    let _f2 = File::open("hello.txt").expect("Failed to open the hello.txt file");
+    // Note:
+    // all unwrap calls that panic print the same message
+    // so use expect will be more concise
+    let _f3 = File::open("hello.txt").unwrap();
 }
