87315fe636c39d2e8a9f4e9612d16c6ec6d59d01
strugglebak <strugglebak@gmail.com>
2021-11-12 16:51:30 +0800

    feat: preventing dangling references with lifetimes

---
+++ validating-references-with-lifetimes/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // dangling reference
+    // let r;
+    // {
+    //     let x = 5;
+    //     // r is referring to has gone out of scope
+    //     r = &x;
+    // }
+
+    // println!("r: {}", r);
+}
