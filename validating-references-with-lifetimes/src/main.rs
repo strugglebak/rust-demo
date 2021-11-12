4d93c8a5a3df2dec04651527a2fb6f436e1a2a91
strugglebak <strugglebak@gmail.com>
2021-11-12 19:59:56 +0800

    feat: lifetime annotations in method definitions

--- validating-references-with-lifetimes/src/main.rs
+++ validating-references-with-lifetimes/src/main.rs
@@ -45,12 +45,28 @@ fn main() {
     struct ImportantExcerpt<'a> {
         part: &'a str,
     }
+
+    impl <'a> ImportantExcerpt<'a> {
+        fn level(&self) -> i32 {
+            3
+        }
+        // we’re not required to annotate the lifetime of the reference
+        // to self because of the first elision rule
+        fn announce_and_return_part(&self, announcement: &str) -> &str {
+            println!("Attention please: {}", announcement);
+            self.part
+        }
+    }
+
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
     let i = ImportantExcerpt {
         part: first_sentence,
     };
     println!("i.part = {}", i.part);
+    println!("i.level() = {}", i.level());
+    i.announce_and_return_part("hello");
+
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -73,7 +89,7 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 //     result.as_str()
 // }
 
-// rules:
+// elision rules:
 // 1. each parameter that is a reference gets its own lifetime parameter
 // ```rs
 // fn foo<'a>(x: &'a i32);
