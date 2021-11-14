4ce7b3f8947f54ef2d9127a86f5abcfaf43efbbf
strugglebak <strugglebak@gmail.com>
2021-11-14 21:09:32 +0800

    feat: testing private functions

--- test-organization/src/lib.rs
+++ test-organization/src/lib.rs
@@ -5,4 +5,18 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
+    use super::*;
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
