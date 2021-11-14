81d865d01aff01afb4708faa1e6ea30f81c034ea
strugglebak <strugglebak@gmail.com>
2021-11-14 11:30:48 +0800

    feat: showing function output

---
+++ controlling-how-tests-are-run/src/lib.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+  // printed values for passing tests
+  // cargo test -- --show-output
+  use super::*;
+
+  #[test]
+  fn this_test_will_pass() {
+    let value = prints_and_returns_10(4);
+    assert_eq!(10, value);
+  }
+
+  #[test]
+  fn this_test_will_fail() {
+    let value = prints_and_returns_10(8);
+    assert_eq!(5, value);
+  }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+  println!("I got the value {}", a);
+  10
+}
