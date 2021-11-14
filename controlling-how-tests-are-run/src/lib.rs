db2f1c7192d0d62b2053b9a572b9fc0098aad7b9
strugglebak <strugglebak@gmail.com>
2021-11-14 11:33:16 +0800

    feat: running single tests

--- controlling-how-tests-are-run/src/lib.rs
+++ controlling-how-tests-are-run/src/lib.rs
@@ -15,9 +15,29 @@ mod tests {
     let value = prints_and_returns_10(8);
     assert_eq!(5, value);
   }
+
+  #[test]
+  fn add_two_and_two() {
+    assert_eq!(4, add_two(2));
+  }
+
+  #[test]
+  fn add_three_and_two() {
+    assert_eq!(5, add_two(3));
+  }
+
+  // cargo test one_hundred
+  #[test]
+  fn one_hundred() {
+    assert_eq!(102, add_two(100));
+  }
 }
 
 fn prints_and_returns_10(a: i32) -> i32 {
   println!("I got the value {}", a);
   10
+}
+
+pub fn add_two(a: i32) -> i32 {
+  a + 2
 }
