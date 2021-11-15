8da694a73f3421977c7c17851221f3f4479361b4
strugglebak <strugglebak@gmail.com>
2021-11-15 22:01:43 +0800

    feat: closure type inference and annotation

--- closures-anonymous-functions-that-can-capture-their-environment/src/main.rs
+++ closures-anonymous-functions-that-can-capture-their-environment/src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
+    let example_closure = |x| x;
+    let _s = example_closure(String::from("hello"));
+    // error:
+    // Those types are then locked into the closure in example_closure
+    // let n = example_closure(5);
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
@@ -12,6 +17,14 @@ fn generate_workout(intensity: u32, random_number: u32) {
     // To define a closure, we start with a pair of vertical pipes (|)
     // Smalltalk and Ruby has the same syntax
     // more parameter should be like `|param1, param2|`
+    // hey’re stored in variables and used without naming them
+    // and exposing them to users of our library.
+
+// fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+// let add_one_v2 = |x: u32| -> u32 { x + 1 };
+// let add_one_v3 = |x|             { x + 1 };
+// let add_one_v4 = |x|               x + 1  ;
+
     let expensive_closure = |num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
