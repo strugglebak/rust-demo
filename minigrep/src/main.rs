e1a222d3c7ea88abc5b063502dd36165b880c8a8
strugglebak <strugglebak@gmail.com>
2021-11-15 19:02:00 +0800

    feat: printing Errors to standard error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,15 +11,12 @@ fn main() {
 
     // Using unwrap_or_else allows us to define some custom, non-panic! error handling
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
