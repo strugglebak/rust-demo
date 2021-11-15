68fd4ceee73f3afc6189d349187c9e90af1e83dc
strugglebak <strugglebak@gmail.com>
2021-11-15 16:22:48 +0800

    feat: reading the argument values

---
+++ minigrep/src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    // iterators produce a series of values,
+    // and we can call the collect method on an iterator to turn it into a collection,
+    // Note that std::env::args will panic if any argument contains invalid Unicode
+    // you can use std::env::args_os instead
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+}
