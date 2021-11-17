40baf3cbfb7042bbf5e00981638c058d6c234a97
strugglebak <strugglebak@gmail.com>
2021-11-17 17:50:44 +0800

    feat: adding a test to a workspace

--- add/add-one/src/lib.rs
+++ add/add-one/src/lib.rs
@@ -2,10 +2,11 @@ use rand;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(3, add_one(2));
     }
 }
 pub fn add_one(x: i32) -> i32 {
