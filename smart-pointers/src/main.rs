8ad3985270cb9ef4dccad1e882849f278db4f30f
strugglebak <strugglebak@gmail.com>
2021-11-18 18:32:09 +0800

    feat: computing the size of a non-recursive type

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,4 +1,16 @@
+use crate::List::{Cons, Nil};
+// error:
+// Rust can’t figure out how much space it needs to store a List value
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
 fn main() {
     let b = Box::new(4);
     println!("b = {}", b);
+
+    // error:
+    // let _list = Cons(1, Cons(2, Cons(3, Nil)));
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
