5288346ad7c88412f71a3d5c75d877d97114bfe3
strugglebak <strugglebak@gmail.com>
2021-11-19 19:33:55 +0800

    feat: interior mutability: a mutatble borrow to an immutable value

--- smart-pointers-rc/src/main.rs
+++ smart-pointers-rc/src/main.rs
@@ -7,6 +7,24 @@ enum List {
     Cons(i32, Rc<List>),
     Nil,
 }
+// The RefCell<T> type is useful when you’re sure your code follows the borrowing
+// rules but the compiler is unable to understand and guarantee that
+//
+// the reasons to choose Box<T>, Rc<T>, or RefCell<T>:
+//
+// 1.
+// Rc<T> enables multiple owners of the same data;
+// Box<T> and RefCell<T> have single owners.
+//
+// 2.
+// Rc<T> allows only immutable borrows checked at compile time;
+// Box<T> allows immutable or mutable borrows checked at compile time;
+// RefCell<T> allows immutable or mutable borrows checked at runtime.
+//
+// 3.
+// Because RefCell<T> allows mutable borrows checked at runtime,
+// you can mutate the value inside the RefCell<T>
+// even when the RefCell<T> is immutable.
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
@@ -19,4 +37,9 @@ fn main() {
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of the scope = {}", Rc::strong_count(&a));
+
+    // let x = 5;
+    // error:
+    // cannot borrow as mutable
+    // let y = &mut x;
 }
