35548a1129c3f0dcd23d368feb504ddfd888a546
strugglebak <strugglebak@gmail.com>
2021-11-21 14:57:47 +0800

    feat: waiting for all threads to finish using join handles

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,15 +2,22 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    // The main thread will wait for
+    // the spawned thread to finish and then run its for loop
+    handle.join().unwrap();
+
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    // make sure the spawned thread finishes before main exits
+    // handle.join().unwrap();
 }
