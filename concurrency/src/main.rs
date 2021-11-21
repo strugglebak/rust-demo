8c4174fbc10e89b9973c5a67d1b2b8202dbc1313
strugglebak <strugglebak@gmail.com>
2021-11-21 15:08:09 +0800

    feat: using move closures with threads

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -20,4 +20,25 @@ fn main() {
 
     // make sure the spawned thread finishes before main exits
     // handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+    // error:
+    // thread::spawn runs this closure in a new thread
+    // we should be able to access v inside that new thread
+
+    // let handle2 = thread::spawn(|| {
+    //     println!("Here's a vector: {:?}", v);
+    // });
+
+    // force the closure to take ownership of the values
+    // it’s using rather than allowing Rust to infer that
+    // it should borrow the values
+    let handle2 = thread::spawn(move|| {
+        println!("Here's a vector: {:?}", v);
+    });
+    // error:
+    // adding move to the closure
+    // we could no longer call drop on it in the main thread
+    // drop(v);
+    handle2.join().unwrap();
 }
