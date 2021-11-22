1065b714e3f01a79353b720ccfb4e5b890071486
strugglebak <strugglebak@gmail.com>
2021-11-22 19:18:38 +0800

    feat: requesting a review of the post changes its state

--- objectorienteddesignpattern/src/lib.rs
+++ objectorienteddesignpattern/src/lib.rs
@@ -1,7 +1,25 @@
 
-trait State {}
+trait State {
+  fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
+struct PendingReview {}
 struct Draft {}
-impl State for Draft {}
+impl State for PendingReview {
+  // self: Box<Self>
+  // This syntax means the method is only valid when called on a Box holding the type
+  // This syntax takes ownership of Box<Self>
+  // invalidating the old state so the state value of the Post can transform into a new state
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    // when we request a review on a post already in the PendingReview state,
+    // it should stay in the PendingReview state
+    Self
+  }
+}
+impl State for Draft {
+  fn request_review(self: Box<Self>) -> Box<dyn State> {
+    Box::new(PendingReview {})
+  }
+}
 
 pub struct Post {
   state: Option<Box<dyn State>>,
@@ -21,5 +39,17 @@ impl Post {
 
   pub fn content(&self) -> &str {
     ""
+  }
+
+  // To consume the old state
+  // the request_review method needs to take ownership of the state value
+  pub fn request_review(&mut self) {
+    // take the Some value out of the state field and leave a None in its place
+    // because Rust doesn’t let us have unpopulated fields in structs
+    // This lets us move the state value out of Post rather than borrowing it
+    if let Some(s) = self.state.take() {
+      // Then we’ll set the post’s state value to the result of this operation
+      self.state = Some(s.request_review())
+    }
   }
 }
