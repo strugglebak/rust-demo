ea6a8e3c76280e238617b8b1afec8dfed77cb25e
strugglebak <strugglebak@gmail.com>
2021-11-24 15:17:05 +0800

    feat: destructuring to break apart values

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -84,6 +84,23 @@ fn main() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let p = Point { x: 0, y: 7 };
+    let Point {x: a, y: b} = p;
+    let Point { x, y } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
