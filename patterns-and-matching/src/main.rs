005170a29ab427d79cf8a971a45c186f3efd697b
strugglebak <strugglebak@gmail.com>
2021-11-24 15:18:49 +0800

    feat: destructuring enums

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -101,6 +101,30 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!(
+                "Move in the x direction {} and in the y direction {}",
+                x, y
+            );
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
