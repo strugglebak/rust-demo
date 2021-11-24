3adc17c4d59a25c277761322f9cad1fe7415f2a8
strugglebak <strugglebak@gmail.com>
2021-11-24 15:22:55 +0800

    feat: ignoring an entire value with _

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -144,8 +144,12 @@ fn main() {
     }
 
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    foo(3, 4);
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
     println!("Current location: ({}, {})", x, y);
+}
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
