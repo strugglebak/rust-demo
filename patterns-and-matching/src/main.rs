a158200101d94e9a128b01a905f34b23faad54b4
strugglebak <strugglebak@gmail.com>
2021-11-24 15:20:58 +0800

    feat: destructuring nested structs and enums

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -106,24 +106,41 @@ fn main() {
         Quit,
         Move { x: i32, y: i32 },
         Write(String),
-        ChangeColor(i32, i32, i32),
+        // ChangeColor(i32, i32, i32),
+        ChangeColor(Color),
     }
-    let msg = Message::ChangeColor(0, 160, 255);
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+    // let msg = Message::ChangeColor(0, 160, 255);
+    // match msg {
+    //     Message::Quit => {
+    //         println!("The Quit variant has no data to destructure.")
+    //     }
+    //     Message::Move { x, y } => {
+    //         println!(
+    //             "Move in the x direction {} and in the y direction {}",
+    //             x, y
+    //         );
+    //     }
+    //     Message::Write(text) => println!("Text message: {}", text),
+    //     Message::ChangeColor(r, g, b) => println!(
+    //         "Change the color to red {}, green {}, and blue {}",
+    //         r, g, b
+    //     ),
+    // }
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.")
-        }
-        Message::Move { x, y } => {
-            println!(
-                "Move in the x direction {} and in the y direction {}",
-                x, y
-            );
-        }
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => println!(
+        Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
             "Change the color to red {}, green {}, and blue {}",
             r, g, b
         ),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
     }
 }
 
