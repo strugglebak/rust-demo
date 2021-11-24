b9fe33ee466c679ee4d385a9b97532b3dc69559d
strugglebak <strugglebak@gmail.com>
2021-11-24 15:05:23 +0800

    feat: matching named variables

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -49,6 +49,17 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
