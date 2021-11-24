de990959294b86d5ed387fb77012e09b4628074e
strugglebak <strugglebak@gmail.com>
2021-11-24 14:28:53 +0800

    feat: function parameters

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -34,4 +34,11 @@ fn main() {
     let (x, y, z) = (1, 2, 3);
     //error:
     // let (x, y) = (1, 2, 3);
+
+    let point = (x, y);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
 }
