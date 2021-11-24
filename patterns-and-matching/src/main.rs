d006f49bb47be48d5c3e20727e17e4308500ef8c
strugglebak <strugglebak@gmail.com>
2021-11-24 15:35:22 +0800

    feat: extra conditionals with match guards

--- patterns-and-matching/src/main.rs
+++ patterns-and-matching/src/main.rs
@@ -200,6 +200,32 @@ fn main() {
     //         println!("Some numbers: {}", second)
     //     },
     // }
+
+    let num = Some(4);
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
