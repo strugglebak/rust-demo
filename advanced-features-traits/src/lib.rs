ead623183a54444add8a50d37cdd6d327ea5b01b
strugglebak <strugglebak@gmail.com>
2021-11-25 20:30:26 +0800

    feat: default generic type parameters and operator overloading

--- advanced-features-traits/src/lib.rs
+++ advanced-features-traits/src/lib.rs
@@ -1,3 +1,19 @@
-pub trait Iterator<T> {
-    fn next(&mut self) -> Option<T>;
+// pub trait Iterator<T> {
+//     fn next(&mut self) -> Option<T>;
+// }
+
+use std::ops::Add;
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+// You’ll use default type parameters in two main ways:
+// 1. To extend a type without breaking existing code
+// 2. To allow customization in specific cases most users won’t need
+impl Add<Meters> for Millimeters {
+  type Output = Millimeters;
+
+  fn add(self, other: Meters) -> Millimeters {
+    Millimeters(self.0 + (other.0 * 1000))
+  }
 }
--- advanced-features-traits/src/main.rs
+++ advanced-features-traits/src/main.rs
@@ -1,3 +1,31 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// The default generic type in this code is within the Add trait
+// trait Add<Rhs=Self> {
+//     type Output;
+
+//     fn add(self, rhs: Rhs) -> Self::Output;
+// }
+
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
 }
