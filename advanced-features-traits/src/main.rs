30a2d19b79348e75fcb5be4364a8e287a27c8803
strugglebak <strugglebak@gmail.com>
2021-11-25 20:51:58 +0800

    feat: fully qualified syntax for disambiguation: calling methods with the same name

--- advanced-features-traits/src/main.rs
+++ advanced-features-traits/src/main.rs
@@ -23,9 +23,74 @@ impl Add for Point {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+trait Wizard {
+    fn fly(&self);
+}
+struct Human;
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+struct Dog;
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 }
     );
+
+    let person = Human;
+    // This is your captain speaking.
+    // Up!
+    // *waving arms furiously*
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+    // A baby dog is called a Spot
+    println!("A baby dog is called a {}", Dog::baby_name());
+    // error:
+    // Because Animal::baby_name is an associated function rather than a method,
+    // and thus doesn’t have a self parameter,
+    // Rust can’t figure out which implementation of Animal::baby_name we want.
+
+    // associated functions that are part of traits don’t have a self parameter
+    // When two types in the same scope implement that trait
+    // Rust can’t figure out which type you mean unless you use fully qualified syntax
+
+    // println!("A baby dog is called a {}", Animal::baby_name());
+
+    // using fully qualified syntax
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+
+    // In general, fully qualified syntax is defined as follows:
+    // <Type as Trait>::function(receiver_if_method, next_arg, ...);
 }
