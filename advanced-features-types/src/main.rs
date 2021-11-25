4ea7aea715cc103a364e44aad9ec0781acc12557
strugglebak <strugglebak@gmail.com>
2021-11-25 22:15:12 +0800

    feat: dynamically sized types and the sized trait

--- advanced-features-types/src/main.rs
+++ advanced-features-types/src/main.rs
@@ -25,7 +25,32 @@ fn main() {
     println!("x + y = {}", x + y);
 
     let f: Thunk = Box::new(|| println!("hi"));
+
+    // error:
+    // Rust needs to know how much memory to allocate for any value of a particular type,
+    // and all values of a type must use the same amount of memory
+    // we make the types of s1 and s2 a &str rather than a str
+    // The golden rule of dynamically sized types is that
+    // we must always put values of dynamically sized types behind a pointer of some kind.
+    // let s1: str = "Hello there!";
+    // let s2: str = "How's it going?";
 }
+
+// Dynamically Sized Types
+// To work with DSTs, Rust has a particular trait called the Sized trait
+// to determine whether or not a type’s size is known at compile time
+// ?Sized means “T may or may not be Sized”
+fn generic<T: ?Sized>(t: &T) {
+    // &T
+    // ecause the type might not be Sized,
+    // we need to use it behind some kind of pointer
+    // --snip--
+}
+fn generic2<T: Sized>(t: T) {
+    // --snip--
+}
+
+
 
 fn f1(f: Thunk) {
 }
