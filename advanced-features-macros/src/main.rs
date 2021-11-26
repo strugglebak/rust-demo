915e5d77eb26142945e5eac6b10acd1d977229cd
strugglebak <strugglebak@gmail.com>
2021-11-26 18:19:28 +0800

    feat: write a custom derive macro

--- advanced-features-macros/src/main.rs
+++ advanced-features-macros/src/main.rs
@@ -27,5 +27,16 @@
 // pub fn some_name(input: TokenStream) -> TokenStream {
 // }
 
+use hello_macro::HelloMacro;
+
+struct Pancakes;
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
+
 fn main() {
+    Pancakes::hello_macro();
 }
