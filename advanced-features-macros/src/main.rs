6ed3f051c74a6641d69b025c64dbc2a92b45c986
strugglebak <strugglebak@gmail.com>
2021-11-26 17:23:16 +0800

    feat: declarative macros with macro_rules! for general metaprogramming

---
+++ advanced-features-macros/src/main.rs
@@ -0,0 +1,26 @@
+// #[macro_export]
+// macro_rules! vec {
+//     ( $( $x:expr ),* ) => {
+//         {
+//             let mut temp_vec = Vec::new();
+//             $(
+//                 temp_vec.push($x);
+//             )*
+//             temp_vec
+//         }
+//     };
+// }
+
+// ↓
+
+// {
+//     let mut temp_vec = Vec::new();
+//     temp_vec.push(1);
+//     temp_vec.push(2);
+//     temp_vec.push(3);
+//     temp_vec
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
