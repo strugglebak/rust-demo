697608bc2040da2791abacad7f4ab7045e563ec0
strugglebak <strugglebak@gmail.com>
2021-11-27 16:05:02 +0800

    feat: reading the request

--- single-threaded-web-server/hello/src/main.rs
+++ single-threaded-web-server/hello/src/main.rs
@@ -1,9 +1,20 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // we declare a buffer on the stack to hold the data that is read in
+    // We’ve made the buffer 1024 bytes in size
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    // takes a &[u8] and produces a String from it
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
