2e5899486f9a8e5364ad3f66ee33632d35e04094
strugglebak <strugglebak@gmail.com>
2021-11-27 21:34:30 +0800

    feat: a worker struct responsible for sending code from the ThreadPool to a thread

--- single-threaded-web-server/hello/src/lib.rs
+++ single-threaded-web-server/hello/src/lib.rs
@@ -1,17 +1,31 @@
 use std::thread;
 
+struct Worker {
+  id: usize,
+  thread: thread::JoinHandle<()>,
+}
+impl Worker {
+  fn new(id: usize) -> Worker {
+    let thread = thread::spawn(|| {});
+    Worker {
+      id,
+      thread,
+    }
+  }
+}
+
 pub struct ThreadPool {
-  threads: Vec<thread::JoinHandle<()>>,
+  workers: Vec<Worker>,
 };
 
 impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
-    let mut threads = Vec::with_capacity(size);
-    for _ in 0..size {
-      // create some threads and store them in the vector
+    let mut workers = Vec::with_capacity(size);
+    for id in 0..size {
+      workers.push(Worker::new(id));
     }
-    ThreadPool { threads }
+    ThreadPool { workers }
   }
 
   pub fn execute<F>(&self, f: F)
