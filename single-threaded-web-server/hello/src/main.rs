7231ae6cd2e68106f9e3c951f1583b5e2ffe477c
strugglebak <strugglebak@gmail.com>
2021-11-27 16:07:38 +0800

    feat: writing a response

--- single-threaded-web-server/hello/src/main.rs
+++ single-threaded-web-server/hello/src/main.rs
@@ -17,4 +17,8 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     // takes a &[u8] and produces a String from it
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
